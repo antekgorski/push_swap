cb257010eff0268e037f5991f8dffe24ceb539a1
Antek Gorski <antek.gorski@wp.pl>
2024-10-30 11:05:56 +0100

    rotete

--- push_swap/list_rotate.c
+++ push_swap/list_rotate.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   list_rotate.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: agorski <agorski@student.42.fr>            +#+  +:+       +#+        */
+/*   By: agorski <agorski@student.42warsaw.pl>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/27 18:18:11 by agorski           #+#    #+#             */
-/*   Updated: 2024/10/27 22:52:16 by agorski          ###   ########.fr       */
+/*   Updated: 2024/10/28 13:34:43 by agorski          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,13 @@
 
 void	rotate(t_element **head)
 {
-	t_element	*be_last;
+	t_element	*shift_up;
 
 	if (!(*head) || !(*head)->next_element)
 		return ;
-	be_last = *head;
-	while (be_last->next_element)
-		be_last = be_last->next_element;
-    ??????????
+	shift_up = *head;
+	while (shift_up->next_element)
+		shift_up = shift_up->next_element;
+	shift_up->next_element = *head
+    
 }
